d9e0545a0b8e0974d660af0cdd42ebc1fc2105ec
DanRodriJD <danrodri.davila@gmail.com>
2025-08-19 04:02:26 +0000

    finalizacion del proyecto

--- src/js/app.js
+++ src/js/app.js
@@ -28,19 +28,79 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  // Redes Sociales
+  let twitter = "";
+  if (variables.twitter) {
+    twitter = `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`;
+  }
+  // if the user has not provided a value for the social media, we don't show it
 
-  // reset the website body with the new html output
+  let instagram = "";
+  if (variables.instagram) {
+    instagram = `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`;
+  }
+
+  let linkedin = "";
+  if (variables.linkedin) {
+    linkedin = `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`;
+  }
+  let github = "";
+  if (variables.github) {
+    github = `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`;
+  }
+  let fullName = "";
+  if (variables.name && variables.lastName) {
+    fullName = `${variables.name} ${variables.lastName}`;
+  } // if both name and lastName are provided, we concatenate them
+  else if (variables.name) {
+    fullName = variables.name;
+  } else if (variables.lastName) {
+    fullName = variables.lastName;
+  } else {
+    fullName = ""; // default name if none provided
+  }
+  let role = "";
+  if (variables.role) {
+    role = variables.role;
+  } else {
+    role = "Web Developer";
+  } // default role if none provided
+  let ubication = "";
+  if (variables.city && variables.country) {
+    ubication = `${variables.city} ${variables.country}`;
+  } else if (variables.city) {
+    ubication = variables.city;
+  } else if (variables.country) {
+    ubication = variables.country;
+  } else {
+    ubication = "Miami, USA"; // default location if none provided
+  }
+
+  // finally we build the HTML with all the variables
+  // we use template literals to make it easier to read and write
+  // we use the variables to fill the content of the widget
+  // we use the class "widget" to style the widget
+  // we use the class "photo" to style the avatar
+  // we use the class "social-media" to style the social media bar
+  // we use the class "position-left" or "position-right" to position the social media bar
+  // we use the class "cover" to style the cover image
+  // we use the class "cover img" to style the cover image
+  // we use the class "widget_content" to style the widget content
+  // we use the class "widget_content h1" to style the name
+  // we use the class "widget_content h2" to style the role
+  // we use the class "widget_content h3" to style the location
+
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${fullName}</h1>
+          <h2>${role}</h2>
+          <h3>${ubication}</h3>
+          <ul class="social-media ${variables.socialMediaPosition}">
+            ${twitter}
+            ${github}
+            ${linkedin}
+            ${instagram}
           </ul>
         </div>
     `;
